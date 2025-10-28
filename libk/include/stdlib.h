3c1f006400bc9b4a5ad9512741ed3e6b48c6849a
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-10-28 16:37:03 +0200

    move libk inside sys

--- libk/include/stdlib.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STDLIB_H
-#define _STDLIB_H 1
-
-#include <sys/cdefs.h>
-
-noreturn void abort(void);
-
-#endif
--- sys/include/stdlib.h
+++ sys/include/stdlib.h
@@ -1,4 +1,8 @@
-#ifndef STDLIB_H
-#define STDLIB_H
+#ifndef _STDLIB_H
+#define _STDLIB_H 1
+
+#include <sys/cdefs.h>
+
+noreturn void abort(void);
 
 #endif
